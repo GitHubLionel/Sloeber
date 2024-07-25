aa762eeb2c9975d381365dde6bc05802efe53b08
Lionel <lionel.reynaud@free.fr>
2024-07-25 18:32:40 +0200

    Add new library MQTT_utils
    A library to simplify MQTT. See the simple project in workspace

---
+++ Library/MQTT_utils/MQTT_utils.cpp
@@ -0,0 +1,186 @@
+#include "MQTT_utils.h"
+
+// Function for debug message, may be redefined elsewhere
+void __attribute__((weak)) print_debug(const String mess, bool ln = true)
+{
+	// Just to avoid compile warning
+	(void) mess;
+	(void) ln;
+}
+
+/**
+ * Default constructor
+ * You then must provide Credential before call begin
+ */
+MQTTClient::MQTTClient()
+{
+	MQTT_client = new PubSubClient(wifiClient);
+
+	using namespace std::placeholders; //for _1, _2, _3
+	auto callback = std::bind(&MQTTClient::mqttCallback, this, _1, _2, _3);
+
+	MQTT_client->setCallback(callback);
+
+	// Create a semaphore to secure MQTT transaction
+	sema_MQTT_KeepAlive = xSemaphoreCreateBinary();
+	xSemaphoreGive(sema_MQTT_KeepAlive);
+}
+
+MQTTClient::MQTTClient(const MQTT_Credential_t mqtt):MQTTClient()
+{
+	SetCredential(mqtt);
+}
+
+void MQTTClient::mqttCallback(char *topic, uint8_t *payload, unsigned int length)
+{
+	memset(Message.payload, '\0', PAYLOAD_MAXSIZE); // clear payload char buffer
+	Message.topic = ""; // clear topic string buffer
+	Message.topic = topic; // store new topic
+
+	// extract payload. Note : string is ended since we have initalized to 0
+	for (int i = 0; i < length; i++)
+	{
+		Message.payload[i] = ((char) payload[i]);
+	}
+
+	// print_debug
+	print_debug("Message arrived in topic: ", false);
+	print_debug(topic);
+	print_debug("Message:", false);
+	print_debug(String(Message.payload));
+	print_debug("-----------------------");
+}
+
+void MQTTClient::SetCredential(const MQTT_Credential_t mqtt)
+{
+	Credential.username = mqtt.username;
+	Credential.password = mqtt.password;
+	Credential.broker = mqtt.broker;
+	Credential.port = mqtt.port;
+}
+
+/**
+ * Establish the connexion with the MQTT broker
+ */
+bool MQTTClient::begin()
+{
+	MQTT_client->setServer(Credential.broker.c_str(), Credential.port);
+
+	client_id = "esp32-client-";
+	client_id += String(WiFi.macAddress());
+	String txt = "The client " + client_id + " connects to the public MQTT brokern";
+	print_debug(txt);
+
+	return Connexion();
+}
+
+bool MQTTClient::begin(const MQTT_Credential_t mqtt)
+{
+	SetCredential(mqtt);
+	return begin();
+}
+
+/**
+ * Connexion to the broker
+ * Try to connect to the broker. Max try = 10 by default
+ */
+bool MQTTClient::Connexion(int trycount)
+{
+	// We must be connected to Wifi !
+	if (!(WiFi.status() == WL_CONNECTED))
+		return false;
+
+	int count = 0;
+	while (!MQTT_client->connected())
+	{
+		if (MQTT_client->connect(client_id.c_str(), Credential.username.c_str(),
+				Credential.password.c_str()))
+		{
+			print_debug("Public MQTT broker connected to Mosquitto");
+			return true;
+		}
+		else
+		{
+			print_debug("failed with state: " + String(MQTT_client->state()));
+			count++;
+			if (count == trycount)
+				break;
+		}
+	}
+	return false;
+}
+
+/**
+ * Publish text in the topic
+ */
+void MQTTClient::Publish(const String topic, const String text, bool subscribe)
+{
+	// Publish and subscribe
+	if (MQTT_client->connected())
+	{
+		xSemaphoreTake(sema_MQTT_KeepAlive, portMAX_DELAY);
+		MQTT_client->publish(topic.c_str(), text.c_str());
+		if (subscribe)
+			MQTT_client->subscribe(topic.c_str());
+		xSemaphoreGive(sema_MQTT_KeepAlive);
+	}
+}
+
+/**
+ * Subscribe to topic if subscribe == true else unsubscribe
+ */
+void MQTTClient::Subscribe(const String topic, bool subscribe)
+{
+	if (MQTT_client->connected())
+	{
+		if (subscribe)
+			MQTT_client->subscribe(topic.c_str());
+		else
+			MQTT_client->unsubscribe(topic.c_str());
+	}
+}
+
+/**
+ * Client loop. Should be call at the end of the main loop
+ * Another solution is to use a the Keepalive_Task to do that
+ */
+bool MQTTClient::Loop(void)
+{
+	if ((wifiClient.connected()) && (WiFi.status() == WL_CONNECTED))
+	{
+		// whiles MQTTlient.loop() is running no other mqtt operations should be in process
+		xSemaphoreTake(sema_MQTT_KeepAlive, portMAX_DELAY);
+		MQTT_client->loop();
+		xSemaphoreGive(sema_MQTT_KeepAlive);
+		return true;
+	}
+	return false;
+}
+
+void MQTTClient::Keepalive_Task(void *parameter)
+{
+	// setting keep alive to 90 seconds makes for a very reliable connection, must be set before the 1st connection is made.
+	MQTT_client->setKeepAlive(90);
+	TickType_t xLastWakeTime = xTaskGetTickCount();
+	const TickType_t xFrequency = 250; //delay for ms
+	for (;;)
+	{
+		if (!Loop())
+		{
+//			log_i("MQTT keep alive found MQTT status % s WiFi status % s", String(wifiClient.connected()), String(WiFi.status()));
+			if (!(wifiClient.connected()) || !(WiFi.status() == WL_CONNECTED))
+			{
+				Wifi_Connexion();
+			}
+			Connexion();
+		}
+		//log_i( " high watermark % d",  uxTaskGetStackHighWaterMark( NULL ) );
+		xLastWakeTime = xTaskGetTickCount();
+		vTaskDelayUntil(&xLastWakeTime, xFrequency);
+	}
+	vTaskDelete( NULL);
+}
+
+// ********************************************************************************
+// End of file
+// ********************************************************************************
---
+++ workspace/ESP32_MQTT/ESP32_MQTT.ino
@@ -0,0 +1,458 @@
+#include "Arduino.h"
+
+// Vérifier que les modifications sont bien prises en compte dans sloeber.ino.cpp
+
+// Pour le display, à définir comme directive dans le projet :
+// OLED_SSD1306 pour le SSD1306  // Need the directive SSD1306_RAM_128 or SSD1306_RAM_132
+// OLED_SSD1327 pour le SSD1327
+// OLED_SH1107 pour le SH1107
+
+#include "Server_utils.h"			  // Some utils functions for the server
+#include "RTCLocal.h"					  // A pseudo RTC software library
+#include "Partition_utils.h"		// Some utils functions for LittleFS/SPIFFS/FatFS
+#include "Display.h"            // Display function
+
+/**
+ * Define de debug
+ * SERIAL_DEBUG	: Message de debug sur le port série (baud 115200)
+ * LOG_DEBUG	: Enregistre le debug dans un fichier log
+ */
+//#define SERIAL_DEBUG
+//#define LOG_DEBUG
+//#define USE_SAVE_CRASH   // Permet de sauvegarder les données du crash
+#include "Debug_utils.h"		// Some utils functions for debug
+
+#include "Tasks_utils.h"
+//String Idle_str = "";
+
+// A cause de l'erreur : Brownout detector was triggered
+#include "soc/soc.h"
+#include "soc/rtc_cntl_reg.h"
+
+#include "MQTT_utils.h"
+
+/**
+ * Ce programme executé par l'ESPxx fait le lien entre l'interface web et le hardware
+ * WEB <--- Wifi ---> ESPxx <--- UART ---> Hardware (BluePill, Nucleo, ...)
+ * La page web est stockée dans l'ESPxx dans sa mémoire FLASH au format LittleFS
+ * L'analyse des messages web se fait via les callback "server.on()"
+ * L'analyse des messages UART se fait via la fonction CheckUARTMessage()
+ * Messages reconnus par défaut (UART) :
+ * - ssid#password pour se connecter
+ * - GET_IP pour obtenir l'adresse IP
+ * - GET_TIME pour avoir l'heure de l'ESP (réponse au format TIME=dd-mm-yyHhh-nn-ssUunix_timestamp)
+ * - GET_DATE= pour avoir l'heure de l'ESP (réponse au format dd-mm-yy#hh:nn:ss#END_DATE\r\n sans balise)
+ * - DATE=DD/MM/YY#hh:mm:ss pour mettre à l'heure l'ESP
+ * - RESET_ESP pour un soft reset de l'ESP
+ * Pour définir d'autre message, utiliser la fonction UserAnalyseMessage
+ * Les messages entrants et sortants doivent être encadrés par les balises BEGIN_DATA et END_DATA
+ *
+ * Trois façon de se connecter via SSID_CONNEXION (section DEFINE NETWORK):
+ * - Automatique : ssid et password codés en dur
+ * - Par programme : Envoyer le message ssid#password par UART ou fichier
+ *
+ * Connexion SoftAP ou réseau
+ * - Access point : Directive ISSOFTAP = true. Dans ce cas l'adresse IP est 192.168.4.1
+ *
+ * L'utilisation de la classe ServerConnexion simplifie la gestion de la connexion :
+ * - définition d'un objet ServerConnexion suivant le type de connexion, SSID, ...
+ * - définition d'une callback à effectuer une fois que la connexion est établie
+ *
+ * La directive USE_NTP_SERVER permet de récupérer l'heure via Internet pour initialiser le RTC Local
+ */
+
+/**
+ * Détail consommation
+ * ESP01 : 72 mA max (250 mA ? : https://arduino-esp8266.readthedocs.io/en/3.0.2/boards.html)
+ * JSN : 8 mA max
+ * DS18B20 : 1.5 mA
+ * Alimentation Meanwell IRM01-3.3 : 300 mA, 1 W
+ * Alimentation Hi-Link HLK-PM03 : 3.3 V, 900 mA, 3 W
+ */
+
+/**
+ * Si on veut que Serial soit initialisé dans le setup dans le cas où l'on veut communiquer
+ * avec un autre périphérique (exemple le Cirrus).
+ * ATTENTION : Si on défini en même temps SERIAL_DEBUG, on peut avoir des comportements indéfinis.
+ */
+//#define USE_UART
+/*********************************************************************************
+ *                    *****  DEFINE NETWORK  *****
+ *********************************************************************************
+ * Définition de où se trouve le SSID et le password :
+ * SSID_CONNEXION : CONN_INLINE = 0, CONN_UART = 1, CONN_FILE = 2, CONN_EEPROM = 3.
+ * Correspondant respectivement à soit en dur dans le programme, soit lecture via UART,
+ * soit lecture fichier, soit lecture EEPROM
+ * ISSOFTAP	  : false = mode Station connecté au réseau présent, true = Mode Access Point (192.168.4.1)
+ */
+#define SSID_CONNEXION	CONN_INLINE
+#define ISSOFTAP	false
+
+// Défini les callback events et les opérations à faire après connexion
+void OnAfterConnexion(void);
+
+#if SSID_CONNEXION == CONN_INLINE
+#if (ISSOFTAP)
+	// Pas de mot de passe
+	ServerConnexion myServer(ISSOFTAP, "SoftAP", "", OnAfterConnexion);
+//	ServerConnexion myServer(ISSOFTAP, "SoftAP", "", IPAddress(192, 168, 4, 2), IPAddress(192, 168, 4, 1));
+	#else
+// Définir en dur le SSID et le pwd du réseau
+ServerConnexion myServer(ISSOFTAP, "TP-Link_8584", "88222462", OnAfterConnexion);
+#endif
+#else
+	// Connexion UART, File or EEPROM
+  ServerConnexion myServer(ISSOFTAP, OnAfterConnexion);
+#endif
+
+// ********************************************************************************
+// Définition user
+// ********************************************************************************
+
+// Définition de l'adresse du port I2C
+#define I2C_ADDRESS	0x3C // ou 0x78
+String UART_Message = "";
+#if defined(OLED_DEFINED)
+// Delay d'extinction du Oled en seconde
+#define OLED_TIMEOUT	600	// 10 minutes
+#endif
+
+// MQTT Broker
+MQTTClient MQTTTest;
+WRAPPER_STATIC_TASK(MQTTTest);
+
+MQTT_Credential_t mqtt = {"", "", "192.168.1.103", 1883};
+String topic = "ESP32_MQTT";
+bool MQTT_Connected = false;
+
+#define USE_TASK_FOR_LOOP
+
+#define RUN_TASK_IDLE	false
+void Task_ShowIdle(void *parameter)
+{
+	BEGIN_TASK_CODE("ShowIdle");
+	for (EVER)
+	{
+		MQTTTest.Publish(topic, TaskList.GetIdleStr().c_str());
+		END_TASK_CODE();
+	}
+}
+
+void Wifi_Connexion(void)
+{
+	myServer.Connexion(false);
+}
+
+/**
+ * Handle the request from web for MQTT server
+ */
+void handleMQTTRequest(CB_SERVER_PARAM);
+
+// ********************************************************************************
+// Functions prototype
+// ********************************************************************************
+
+// UART message
+bool UserAnalyseMessage(void);
+void handleOperation(CB_SERVER_PARAM);
+
+// ********************************************************************************
+// Initialization
+// ********************************************************************************
+
+/**
+ * The setup function is called once at startup of the sketch
+ * Initialisation de :
+ * 0- serial
+ * 1- littleFS
+ * 2- datetime
+ * 3- ...
+ */
+void setup()
+{
+	// To know the time required for the setup
+	uint32_t start_time = millis();
+
+	// **** 0- initialisation Serial, debug ou pas ****
+	SERIAL_Initialization();
+
+	// **** 1- initialisation LittleFS ****
+	// Start the SPI Flash Files System, abort if not success
+	if (!FS_Partition->begin())
+	{
+		print_debug(F("LittleFS/SPIFFS/FatFS mount failed"));
+		// On abandonne l'initialisation
+		return;
+	}
+
+	// Message de démarrage à mettre APRES SERIAL et LittleFS !
+	print_debug(F("\r\n\r\n****** Starting : "), false);
+	print_debug(getSketchName(__FILE__, true));
+
+	// Création d'heure pseudo RTC. Utilise RTC_Local.
+#ifdef ESP8266
+	FS_Partition->setTimeCallback(RTC_Local_Callback);
+#endif
+
+#ifdef USE_SAVE_CRASH
+	init_and_print_crash();
+#endif
+
+#ifdef SERIAL_DEBUG
+	// Des infos sur la flash
+	ESPinformations();
+	Partition_Info();
+	Partition_ListDir();
+#endif
+
+	// **** 2- initialisation datetime ****
+	// Essaye de récupérer la dernière heure, utile pour un reboot
+	// Paramètres éventuellement à adapter : AutoSave et SaveDelay (par défaut toutes les 10 s)
+	RTC_Local.setupDateTime();
+
+	// **** 3- Initialisation du display
+	if (IHM_Initialization(I2C_ADDRESS, false))
+		print_debug(F("Display Ok"));
+	IHM_TimeOut_Display(OLED_TIMEOUT);
+
+	// A cause de l'erreur : Brownout detector was triggered
+//	WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0);
+
+	IHM_Print0("Connexion .....");
+	print_debug(F("==> Wait for network <=="));
+	if (!myServer.WaitForConnexion((Conn_typedef) SSID_CONNEXION))
+		print_debug(F("==> Connected to network <=="));
+	else
+	{
+		IHM_Print0("Failed !   ");
+		print_debug(F("==> Connexion to network failed <=="));
+	}
+
+	// **** FIN- Attente connexion réseau
+	print_debug("*** Setup time : " + String(millis() - start_time) + " ms ***\r\n");
+
+	// IDLE Task
+	TaskList.AddTask( {MQTT_Connected, "ShowIdle", 5000, 5, 5000, CoreAny, Task_ShowIdle});
+	// MQTT Keep alive task
+#ifdef USE_TASK_FOR_LOOP
+	TaskList.AddTask(MQTT_STANDARD_KEEPALIVE_TASK_DATA(false));
+#endif
+	TaskList.Create(RUN_TASK_IDLE);
+}
+
+// The loop function is called in an endless loop
+// If empty, about 50000 loops by second
+// Les mesures ne sont faites que si au moins une seconde est passée
+void loop()
+{
+	bool uptodate = RTC_Local.update();
+
+	// Exemple analyse d'un message UART
+	if (CheckUARTMessage())
+	{
+		if (!myServer.BasicAnalyseMessage())
+			UserAnalyseMessage();
+	}
+
+	// Opération à faire si on est à l'heure : par exemple afficher l'heure
+	if (uptodate)
+	{
+		IHM_Print0(RTC_Local.the_time);
+
+		// Afficher Idle
+		IHM_Print(3, TaskList.GetIdleStr().c_str(), true);
+
+		// Test extinction de l'écran
+		IHM_CheckTurnOff();
+
+//		print_debug(Idle_str);
+	}
+
+	// Listen for HTTP requests from clients
+#ifndef USE_ASYNC_WEBSERVER
+	server.handleClient();
+#endif
+
+	// MQTT
+#ifndef USE_TASK_FOR_LOOP
+	MQTTTest.Loop();
+#endif
+
+	// Temporisation à adapter
+	delay(100);
+}
+
+// ********************************************************************************
+// OnAfterConnexion
+// ********************************************************************************
+/**
+ * Définition des évènements après la connexion au serveur
+ * Définition des opérations à faire juste après la connexion
+ * Cette fonction ne sera appelée qu'une seule fois.
+ * Cette fonction répond aux messages de la fonction ESP_Request du fichier JavaScript ESPTemplate.js
+ */
+void OnAfterConnexion(void)
+{
+	// Server default events
+	Server_CommonEvent(
+			Ev_LoadPage | Ev_GetFile | Ev_DeleteFile | Ev_UploadFile | Ev_ListFile | Ev_ResetESP
+					| Ev_SetTime | Ev_GetTime | Ev_SetDHCPIP | Ev_ResetDHCPIP);
+
+	// Server specific events (voir le javascript)
+	server.on("/getUARTData", HTTP_GET, [](CB_SERVER_PARAM)
+	{
+		pserver->send(200, "text/plain", UART_Message);
+	});
+
+	server.on("/getLastData", HTTP_GET, [](CB_SERVER_PARAM)
+	{
+		pserver->send(200, "text/plain", (String(RTC_Local.the_time) + '#' + UART_Message));
+	});
+
+	server.on("/operation", HTTP_PUT, handleOperation);
+	server.on("/operation", HTTP_GET, handleOperation);
+
+	// MQTT
+	server.on("/MQTT_Request", HTTP_POST, handleMQTTRequest);
+
+	// Begin the server
+	server.begin();
+
+	// Affichage ip adresse
+	IHM_IPAddress(myServer.IPaddress().c_str());
+}
+
+// ********************************************************************************
+// Web/UART functions
+// ********************************************************************************
+
+bool UserAnalyseMessage(void)
+{
+	// Default
+	// Retour à UART
+	printf_message_to_UART("Unknown message : " + String((const char*) UART_Message_Buffer));
+	// Message pour la page web
+	UART_Message = String((const char*) UART_Message_Buffer);
+
+	return false;
+}
+
+void handleOperation(CB_SERVER_PARAM)
+{
+	// Default
+	RETURN_BAD_ARGUMENT();
+
+	print_debug("Operation: " + pserver->arg((int) 0) + "=" + pserver->arg((int) 0));
+
+#if defined(OLED_DEFINED)
+	if (pserver->hasArg("Toggle_Oled"))
+	{
+		IHM_ToggleDisplay();
+	}
+#endif
+
+	pserver->send(204, "text/plain", "");
+}
+
+void handleMQTTRequest(CB_SERVER_PARAM)
+{
+	RETURN_BAD_ARGUMENT();
+
+	// Return si Request n'est pas présent
+	if (!pserver->hasArg("Request"))
+		return;
+
+	String request = pserver->arg("Request");
+
+	// Eviter de spammer le log
+	if (!(request.equals("Data")))
+		print_debug("Request for MQTT broker: ");
+
+	// Connexion request
+	if (request.equals("Connexion"))
+	{
+		print_debug("Connect MQTT IP: " + pserver->arg("IP"));
+		mqtt.broker = pserver->arg("IP");
+		if (MQTT_Connected)
+		{
+			// On est déjà connecté
+			pserver->send(200, "text/plain", "Connected");
+		}
+		else
+		{
+			MQTT_Connected = MQTTTest.begin(mqtt);
+			if (MQTT_Connected)
+			{
+				MQTTTest.Publish(topic, "Hi, I'm ESP32", true);
+				// Exemple, on crée une task
+#if RUN_TASK_IDLE == true
+				TaskList.CreateTask("ShowIdle");
+#endif
+#ifdef USE_TASK_FOR_LOOP
+				TaskList.CreateTask(MQTTTest.GetKeepaliveTaskName());
+#endif
+				pserver->send(200, "text/plain", "Connected");
+			}
+			else
+				pserver->send(200, "text/plain", "Erreur");
+		}
+		return;
+	}
+
+	// Publish request
+	if (request.equals("Publish"))
+	{
+		if (pserver->hasArg("Topic"))
+			topic = pserver->arg("Topic");
+		String Message = "";
+		if (pserver->hasArg("Message"))
+			Message = pserver->arg("Message");
+		bool subscribe = false;
+		if (pserver->hasArg("Subscribe"))
+			subscribe = (pserver->arg("Subscribe")).equals("true");
+		print_debug("Topic: " + topic + " Message: " + Message);
+
+		if (MQTT_Connected)
+		{
+			MQTTTest.Publish(topic, Message, subscribe);
+		}
+
+		pserver->send(204, "text/plain", "");
+		return;
+	}
+
+	// Subscribe request
+	if (request.equals("Subscribe"))
+	{
+		String newtopic = "";
+		print_debug("Subsribe to MQTT: ", false);
+		if (pserver->hasArg("Topic"))
+			newtopic = pserver->arg("Topic");
+
+		print_debug("Topic: " + newtopic);
+
+		if (MQTT_Connected && !newtopic.isEmpty())
+		{
+			MQTTTest.Subscribe(newtopic, true);
+		}
+
+		pserver->send(204, "text/plain", "");
+		return;
+	}
+
+	// Data request
+	if (request.equals("Data"))
+	{
+		if (MQTT_Connected)
+		{
+			String data = MQTTTest.GetMessage().topic + ": " + String(MQTTTest.GetMessage().payload);
+			pserver->send(200, "text/plain", data.c_str());
+		}
+		else
+			pserver->send(200, "text/plain", "Not connected");
+	}
+}
+
+// ********************************************************************************
+// End of file
+// ********************************************************************************
