12618322994f86119d94d62eae0411f7e20647f7
Lionel <lionel.reynaud@free.fr>
2025-01-01 15:04:30 +0100

    Change file name and make class

--- Library/Emul_PV/Emul_PV.cpp
+++ Library/Emul_PV/Emul_PV.cpp
@@ -1,42 +1,30 @@
-#include "PV_data.h"
+#include "Emul_PV.h"
 #include <stdio.h>
 #include <math.h>
 #include "rayonnement.h"
 #include "RTCLocal.h"
 
-static TGPSPosition PV_GPS;
-static TSite PV_Site;
+EmulPV_Class::EmulPV_Class()
+{
+	// default value
+	PV_NOCT = 44;
+	PV_TempCoeffPuissance = -0.41;
+	Ond_Rendement = 95.7;
+	Ond_PuissanceACMax = 1;
+	PV_Puissance = 1;
+	SetRendement();
+}
 
-static double MaskMatin, MaskSoir;
-static bool Mask = false;
-
-// Energie solaire du jour
-static double ESolarDay;
-static TDateTime SunRise, SunSet;
-
-static double PV_Puissance;
-
-static double PV_NOCT, PV_TempCoeffPuissance;
-static double Ond_PuissanceACMax, Ond_Rendement;
-
-static double Rendement;
-bool UsePOnduleurACMax = false;
-
-// Private functions
-void SetRendement(void);
-double ComputeCellTemperature(double aTAmbiante, double aIrradiance);
-
-//PV_data = {{43.49333333, -6.36166666, 209, 0, 5, 800, 0, 0, -0.41, 44, 700, 95.7}};
 /**
  * Initialisation à partir d'un fichier de type ini.
  * Si le fichier n'existe pas, il est créé.
  */
-void PV_Init_From_File(const char *file, PVSite_Struct *PV_data)
+void EmulPV_Class::Init_From_File(const char *file)
 {
 	IniFiles init_file = IniFiles(file);
 	bool exist = init_file.Begin(true);
 
-	PV_Init_From_IniData(PV_data, init_file);
+	Init_From_IniData(init_file);
 
 	if (!exist)
 		init_file.SaveFile("");
@@ -45,42 +33,46 @@ void PV_Init_From_File(const char *file, PVSite_Struct *PV_data)
 /**
  * Initialisation à partir d'un fichier de type ini déjà ouvert en mémoire.
  */
-void PV_Init_From_IniData(PVSite_Struct *PV_data, IniFiles &init_file)
+void EmulPV_Class::Init_From_IniData(IniFiles &init_file)
 {
-	PV_data->latitude = init_file.ReadFloat("GPS", "Latitude", 43.49333333);
-	PV_data->longitude = init_file.ReadFloat("GPS", "Longitude", -6.36166666);
-	PV_data->altitude = init_file.ReadFloat("GPS", "Altitude", 209);
-	PV_data->orientation = init_file.ReadFloat("GPS", "Orientation", 0);
-	PV_data->inclinaison = init_file.ReadFloat("GPS", "Inclinaison", 5);
+	PVSite_Struct dataPV;
 
-	PV_data->PV_puissance = init_file.ReadFloat("PV", "Puissance", 1000);
-	PV_data->PV_Coeff_Puissance = init_file.ReadFloat("PV", "Coeff_Puissance", -0.41);
-	PV_data->PV_Noct = init_file.ReadFloat("PV", "NOCT", 44);
-	PV_data->Ond_PowerACMax = init_file.ReadFloat("PV", "PowerACMax", 1000);
-	PV_data->Ond_Rendement = init_file.ReadFloat("PV", "Rendement", 95.7);
+	dataPV.latitude = init_file.ReadFloat("GPS", "Latitude", 43.49333333);
+	dataPV.longitude = init_file.ReadFloat("GPS", "Longitude", -6.36166666);
 
-	PV_data->Mask_Matin = init_file.ReadFloat("MASK", "Mask_Matin", 0);
-	PV_data->Mask_Soir = init_file.ReadFloat("MASK", "Mask_Soir", 0);
+	dataPV.altitude = init_file.ReadFloat("GPS", "Altitude", 209);
+	dataPV.orientation = init_file.ReadFloat("GPS", "Orientation", 0);
+	dataPV.inclinaison = init_file.ReadFloat("GPS", "Inclinaison", 5);
 
-	PV_Init_From_Array(*PV_data);
+	dataPV.PV_puissance = init_file.ReadFloat("PV", "Puissance", 1000);
+	dataPV.PV_Coeff_Puissance = init_file.ReadFloat("PV", "Coeff_Puissance", -0.41);
+	dataPV.PV_Noct = init_file.ReadFloat("PV", "NOCT", 44);
+	dataPV.Ond_PowerACMax = init_file.ReadFloat("PV", "PowerACMax", 1000);
+	dataPV.Ond_Rendement = init_file.ReadFloat("PV", "Rendement", 95.7);
+
+	dataPV.Mask_Matin = init_file.ReadFloat("MASK", "Mask_Matin", 0);
+	dataPV.Mask_Soir = init_file.ReadFloat("MASK", "Mask_Soir", 0);
+
+	Init_From_Array(dataPV);
 }
 
-void PV_Save_To_File(PVSite_Struct data, IniFiles &init_file)
+void EmulPV_Class::Save_To_File(IniFiles &init_file)
 {
-	init_file.WriteFloat("GPS", "Latitude", data.latitude, "");
-	init_file.WriteFloat("GPS", "Longitude", data.longitude, "");
-	init_file.WriteFloat("GPS", "Altitude", data.altitude, "");
-	init_file.WriteFloat("GPS", "Orientation", data.orientation, "");
-	init_file.WriteFloat("GPS", "Inclinaison", data.inclinaison, "");
+	init_file.WriteFloat("GPS", "Latitude", PV_Data.latitude, "");
+	init_file.WriteFloat("GPS", "Longitude", PV_Data.longitude, "");
 
-	init_file.WriteFloat("PV", "Puissance", data.PV_puissance, "");
-	init_file.WriteFloat("PV", "Coeff_Puissance", data.PV_Coeff_Puissance, "");
-	init_file.WriteFloat("PV", "NOCT", data.PV_Noct, "");
-	init_file.WriteFloat("PV", "PowerACMax", data.Ond_PowerACMax, "");
-	init_file.WriteFloat("PV", "Rendement", data.Ond_Rendement, "");
+	init_file.WriteFloat("GPS", "Altitude", PV_Data.altitude, "");
+	init_file.WriteFloat("GPS", "Orientation", PV_Data.orientation, "");
+	init_file.WriteFloat("GPS", "Inclinaison", PV_Data.inclinaison, "");
 
-	init_file.WriteFloat("MASK", "Mask_Matin", data.Mask_Matin, "");
-	init_file.WriteFloat("MASK", "Mask_Soir", data.Mask_Soir, "");
+	init_file.WriteFloat("PV", "Puissance", PV_Data.PV_puissance, "");
+	init_file.WriteFloat("PV", "Coeff_Puissance", PV_Data.PV_Coeff_Puissance, "");
+	init_file.WriteFloat("PV", "NOCT", PV_Data.PV_Noct, "");
+	init_file.WriteFloat("PV", "PowerACMax", PV_Data.Ond_PowerACMax, "");
+	init_file.WriteFloat("PV", "Rendement", PV_Data.Ond_Rendement, "");
+
+	init_file.WriteFloat("MASK", "Mask_Matin", PV_Data.Mask_Matin, "");
+	init_file.WriteFloat("MASK", "Mask_Soir", PV_Data.Mask_Soir, "");
 
 	init_file.SaveFile("");
 }
@@ -88,16 +80,20 @@ void PV_Save_To_File(PVSite_Struct data, IniFiles &init_file)
 /**
  * Initialiation des données du site : position, caractéristiques, ...
  * Les données sont transmises dans une structure tableau de dimension 12 qui doit contenir dans l'ordre
- * GPS : Latitude, Longitude (négatif à l'Est de Greenwitch), Altitude
+ * GPS : Latitude (Nord), Longitude (négatif à l'Est du méridien de Greenwitch), Altitude
  * Installation : Orientation, Inclinaison, Puissance totale Wc
  * Masque : MaskMatin, MaskSoir (au format DateTime, 0 si pas de masque)
  * Module : coefficient de perte de puissance en température (-0.45 par défaut), NOCT (45 par défaut)
  * Onduleur : la puissance AC max de l'onduleur pour l'écrétage, rendement onduleur (pourcentage)
+ * Exemple PV_data = {{43.49333333, -6.36166666, 209, 0, 5, 800, 0, 0, -0.41, 44, 700, 95.7}};
  */
-void PV_Init_From_Array(PVSite_Struct data)
+void EmulPV_Class::Init_From_Array(const PVSite_Struct &data)
 {
+	PV_Data = data;
+
 	PV_GPS.Latitude = data.Tab[0];
 	PV_GPS.Longitude = data.Tab[1];
+
 	PV_GPS.Altitude = data.Tab[2];
 	PV_GPS.Orientation = data.Tab[3];
 	PV_GPS.Inclinaison = data.Tab[4];
@@ -129,7 +125,7 @@ void PV_Init_From_Array(PVSite_Struct data)
  * 	temp : température
  * 	hr : humidité relative en pourcentage (0.75 par défaut)
  */
-void PV_Set_DayParameters(TAngstromCoeff ang, double temp, double hr)
+void EmulPV_Class::Set_DayParameters(TAngstromCoeff ang, double temp, double hr)
 {
 	PV_Site.HR = hr;
 	if (PV_Site.HR <= 0)
@@ -145,7 +141,7 @@ void PV_Set_DayParameters(TAngstromCoeff ang, double temp, double hr)
 /**
  * Initialise les données du jour commune (mask, énergie solaire)
  */
-void PV_Day_Init(TDateTime day)
+void EmulPV_Class::Day_Init()
 {
 	// Mask
 	Mask = (PV_Site.MaskMatin != 0) || (PV_Site.MaskSoir != 0) || (PV_Site.UseMask);
@@ -156,17 +152,17 @@ void PV_Day_Init(TDateTime day)
 	}
 
 	// L'énergie du jour
-	ESolarDay = ESol(DayOfTheYear(day));
+	ESolarDay = ESol(DayOfTheYear(CurrentDay));
 
 	// Le lever et coucher du Soleil
-	SunRise = Sun_Hour_GPS(day, PV_GPS, sun_rise);
-	SunSet = Sun_Hour_GPS(day, PV_GPS, sun_set);
+	SunRise = Sun_Hour_GPS(CurrentDay, PV_GPS, sun_rise);
+	SunSet = Sun_Hour_GPS(CurrentDay, PV_GPS, sun_set);
 }
 
 /**
  * Renvoie l'heure du lever du Soleil à l'heure solaire ou local
  */
-TDateTime PV_Get_SunRise(bool toLocalTime)
+TDateTime EmulPV_Class::getSunRise(bool toLocalTime)
 {
 	TDateTime dt = SunRise;
 	if (toLocalTime)
@@ -177,7 +173,7 @@ TDateTime PV_Get_SunRise(bool toLocalTime)
 /**
  * Renvoie l'heure du coucher du Soleil à l'heure solaire ou local
  */
-TDateTime PV_Get_SunSet(bool toLocalTime)
+TDateTime EmulPV_Class::getSunSet(bool toLocalTime)
 {
 	TDateTime dt = SunSet;
 	if (toLocalTime)
@@ -188,7 +184,7 @@ TDateTime PV_Get_SunSet(bool toLocalTime)
 /**
  * Renvoie l'heure du transit (midi) du Soleil à l'heure solaire ou local
  */
-TDateTime PV_Get_SunTransit(TDateTime day, bool toLocalTime)
+TDateTime EmulPV_Class::getSunTransit(TDateTime day, bool toLocalTime)
 {
 	TDateTime dt = Sun_Hour_GPS(day, PV_GPS, sun_transit);
 	if (toLocalTime)
@@ -200,26 +196,48 @@ TDateTime PV_Get_SunTransit(TDateTime day, bool toLocalTime)
  * Renvoie une chaine contenant l'heure de lever et coucher du Soleil
  * La chaine SunRise_SunSet doit être définie avec 20 caractères
  */
-void Sun_Rise_Set_Str(char *SunRise_SunSet)
+char* EmulPV_Class::SunRise_SunSet(void)
 {
 	char rise[9], set[9];
 
-	DateTimeToTimeStr(PV_Get_SunRise(true), rise);
-	DateTimeToTimeStr(PV_Get_SunSet(true), set);
-	sprintf(SunRise_SunSet, "%s - %s", rise, set);
+	DateTimeToTimeStr(getSunRise(true), rise);
+	DateTimeToTimeStr(getSunSet(true), set);
+	sprintf(Sun_Rise_Sun_Set, "%s - %s", rise, set);
+	return Sun_Rise_Sun_Set;
+}
+
+/**
+ * Renvoie la data
+ */
+float EmulPV_Class::getData(PVData_Enum data)
+{
+	return PV_Data.Tab[data];
+}
+
+String EmulPV_Class::getData_str(PVData_Enum data)
+{
+	char buffer[10] = {0};
+
+	if ((data == pvLatitude) || (data == pvLongitude))
+		sprintf(buffer, "%.6f", PV_Data.Tab[data]);
+	else
+		sprintf(buffer, "%.2f", PV_Data.Tab[data]);
+
+	return (String) buffer;
 }
 
 /**
  * Renvoie la date du jour au format TDateTime
  */
-TDateTime Now_DateTime(void)
+void EmulPV_Class::setDateTime(void)
 {
 	uint8_t day, month, year;
 	TDateTime Date;
 
 	RTC_Local.getDate(&day, &month, &year);
 	EncodeDate(2000 + year, month, day, &Date);
-	return Date;
+	CurrentDay = Date;
+	Day_Init();
 }
 
 /**
@@ -227,7 +245,7 @@ TDateTime Now_DateTime(void)
  * Les données du jour doivent être initialisées avant (appel de PV_Day_Init)
  * Si on a une heure locale, on doit d'abord la convertir en heure solaire (appel de LocalTimeToSunHour)
  */
-double PV_Irradiance(TDateTime aDateSun)
+double EmulPV_Class::Irradiance(TDateTime aDateSun)
 {
 	double hauteur, azimuth;
 	double lRSDirect;
@@ -238,7 +256,8 @@ double PV_Irradiance(TDateTime aDateSun)
 	if ((aDateSun < SunRise) || (aDateSun > SunSet))
 		return 0.0;
 
-	Sun_Position_Horizontal(aDateSun, PV_Site.GPS.Latitude, PV_Site.GPS.Longitude, &hauteur, &azimuth);
+	Sun_Position_Horizontal(aDateSun, PV_Site.GPS.Latitude, PV_Site.GPS.Longitude, &hauteur,
+			&azimuth);
 	if (hauteur > 0)
 	{
 		// Calcul du diffus
@@ -256,8 +275,11 @@ double PV_Irradiance(TDateTime aDateSun)
 		}
 		if (!masked)
 		{
-			lRSDirect = RSDirect(ESolarDay, hauteur, PV_Site.GPS.Altitude, PV_Site.Angstrom, PV_Site.Temperature, PV_Site.HR);
-			result += lRSDirect * Coefficient_Incidence(hauteur, azimuth, PV_Site.GPS.Inclinaison, PV_Site.GPS.Orientation);
+			lRSDirect = RSDirect(ESolarDay, hauteur, PV_Site.GPS.Altitude, PV_Site.Angstrom,
+					PV_Site.Temperature, PV_Site.HR);
+			result += lRSDirect
+					* Coefficient_Incidence(hauteur, azimuth, PV_Site.GPS.Inclinaison,
+							PV_Site.GPS.Orientation);
 		}
 
 		if (result < 0)
@@ -274,7 +296,7 @@ double PV_Irradiance(TDateTime aDateSun)
 // La surface totale = Surface un module * Nb module
 // On voit qu'on peut simplifier par la surface d'un module
 // Puissance = Irradiance * (Puissance/1000) * (Rendement onduleur) * Nb module
-void SetRendement(void)
+void EmulPV_Class::SetRendement(void)
 {
 	Rendement = PV_Puissance / 1000.0; // Ici Puissance = Puissance module * Nb module
 //  if FRendement = 0 then
@@ -282,7 +304,10 @@ void SetRendement(void)
 	Rendement = Rendement * (Ond_Rendement / 100.0);
 }
 
-double ComputeCellTemperature(double aTAmbiante, double aIrradiance)
+/**
+ * Calcul de la température cellule basé sur le NOCT
+ */
+double EmulPV_Class::ComputeCellTemperature(double aTAmbiante, double aIrradiance)
 {
 	return aTAmbiante + aIrradiance / 800.0 * (PV_NOCT - 20.0);
 }
@@ -292,18 +317,42 @@ double ComputeCellTemperature(double aTAmbiante, double aIrradiance)
  * Les données du jour doivent être initialisées avant (appel de PV_Day_Init)
  * Si on a une heure locale, on doit d'abord la convertir en heure solaire (appel de LocalTimeToSunHour)
  */
-double PV_Power(TDateTime aDateSun)
+double EmulPV_Class::Power(TDateTime aDateSun)
 {
-	double Irradiance, TempCoeff, Puissance;
+	double _Irradiance, TempCoeff, Puissance;
 
-	Irradiance = PV_Irradiance(aDateSun);
+	_Irradiance = Irradiance(aDateSun);
 
-	TempCoeff = 1.0 + PV_TempCoeffPuissance * (ComputeCellTemperature(PV_Site.Temperature, Irradiance) - 25.0);
-	Puissance = Irradiance * Rendement * TempCoeff;
+	TempCoeff = 1.0
+			+ PV_TempCoeffPuissance * (ComputeCellTemperature(PV_Site.Temperature, _Irradiance) - 25.0);
+	Puissance = _Irradiance * Rendement * TempCoeff;
 	if (UsePOnduleurACMax && (Puissance > Ond_PuissanceACMax))
 		Puissance = Ond_PuissanceACMax;
 
 	return Puissance;
+}
+
+/**
+ * Calcul de la puissance théorique à une heure donnée (en seconde) du jour courant
+ */
+double EmulPV_Class::Compute_Power_TH(uint32_t daytime_s, bool summer_hour)
+{
+	TDateTime dt;
+	int32_t time_s = daytime_s;
+
+	// Décalage heure solaire
+	if (summer_hour)
+		time_s -= 7200;
+	else
+		time_s -= 3600;
+
+	if (time_s > 0)
+	{
+		dt = IncSecond(CurrentDay, time_s);
+		return Power(dt);
+	}
+	else
+		return 0.0;
 }
 
 // ********************************************************************************
---
+++ Library/Emul_PV/Emul_PV.h
@@ -0,0 +1,128 @@
+#pragma once
+#ifndef __PV_DATA_H
+#define __PV_DATA_H
+
+#ifdef USE_CONFIG_LIB_FILE
+#include "config_lib.h"
+#endif
+
+#include "Arduino.h"
+
+#include <stdint.h>
+#include <stdbool.h>
+#include "misc.h"
+#include "rayonnement.h"
+#include "IniFiles.h"
+
+#define PVDATA_MAX   12
+
+typedef union
+{
+		float Tab[PVDATA_MAX];
+		struct
+		{
+				// GPS
+				float latitude;  // Nord positif
+				float longitude; // Est négatif de Greenwitch
+				float altitude;
+
+				// Installation
+				float orientation;
+				float inclinaison;
+				float PV_puissance;
+
+				// Masques
+				float Mask_Matin;
+				float Mask_Soir;
+
+				// Module
+				float PV_Coeff_Puissance;
+				float PV_Noct;
+
+				// Onduleur
+				float Ond_PowerACMax;
+				float Ond_Rendement;
+		};
+} PVSite_Struct;
+
+typedef enum
+{
+	pvLatitude,  // Nord positif
+	pvLongitude, // Est négatif de Greenwitch
+	pvAltitude,
+
+	// Installation
+	pvOrientation,
+	pvInclinaison,
+	pvPV_Puissance,
+
+	// Masques
+	pvMask_Matin,
+	pvMask_Soir,
+
+	// Module
+	pvCoeff_Puissance,
+	pvNoct,
+
+	// Onduleur
+	pvOnd_PowerACMax,
+	pvOnd_Rendement,
+
+	// End of data
+	pvMAXDATA
+} PVData_Enum;
+
+class EmulPV_Class
+{
+	public:
+		EmulPV_Class();
+
+		void Init_From_Array(const PVSite_Struct &data);
+		void Init_From_File(const char *file);
+		void Init_From_IniData(IniFiles &init_file);
+		void Save_To_File(IniFiles &init_file);
+		void Set_DayParameters(TAngstromCoeff ang, double temp, double hr);
+
+		TDateTime getSunRise(bool toLocalTime);
+		TDateTime getSunSet(bool toLocalTime);
+		TDateTime getSunTransit(TDateTime day, bool toLocalTime);
+		char *SunRise_SunSet(void);
+		void setDateTime(void);
+
+		float getData(PVData_Enum data);
+		String getData_str(PVData_Enum data);
+
+		double Irradiance(TDateTime aDateSun);
+		double Power(TDateTime aDateSun);
+		double Compute_Power_TH(uint32_t daytime_s, bool summer_hour);
+
+	private:
+		TGPSPosition PV_GPS;
+		TSite PV_Site;
+		PVSite_Struct PV_Data;
+
+		double MaskMatin = 0, MaskSoir = 0;
+		bool Mask = false;
+
+		// Energie solaire du jour
+		double ESolarDay = 0;
+		TDateTime SunRise = 0, SunSet = 0;
+
+		double PV_Puissance;
+
+		double PV_NOCT, PV_TempCoeffPuissance;
+		double Ond_PuissanceACMax, Ond_Rendement;
+
+		double Rendement;
+		bool UsePOnduleurACMax = false;
+
+		TDateTime CurrentDay = 0;
+		char Sun_Rise_Sun_Set[20] = {0};
+
+		void Day_Init();
+		void SetRendement(void);
+		double ComputeCellTemperature(double aTAmbiante, double aIrradiance);
+};
+
+
+#endif /* __PV_DATA_H */
